b24c512cadcc84ba22a3dfb83a379080162979d0
thdrjsdn98 <74442674+thdrjsdn98@users.noreply.github.com>
2026-09-07 18:19:44 +0900

    Update sw.js

--- electrical-standards/sw.js
+++ electrical-standards/sw.js
@@ -1,5 +1,5 @@
 // 간단한 오프라인 캐시용 서비스워커 (네트워크 우선 방식)
-const CACHE_NAME = 'electric-standards-cache-v1';
+const CACHE_NAME = 'electric-standards-cache-v2';
 const FILES_TO_CACHE = [
   './',
   './index.html',
