1a8339897acf112678475449ecf4fb2bb4b1c4fb
thdrjsdn98 <74442674+thdrjsdn98@users.noreply.github.com>
2026-09-08 12:53:38 +0900

    Add files via upload

---
+++ control-eng/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'electric-control-cache-v1';
+const FILES_TO_CACHE = [
+  './', './index.html', './style.css', './script.js', './questions.js',
+  './manifest.json', './icon-192.png', './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(FILES_TO_CACHE); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(key) { return key !== CACHE_NAME; }).map(function(key) { return caches.delete(key); }));
+    })
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      if (response && response.status === 200 && event.request.method === 'GET') {
+        var responseClone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, responseClone); });
+      }
+      return response;
+    }).catch(function() { return caches.match(event.request); })
+  );
+});
