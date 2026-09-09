ebc18e9f01dd3eb85612fd85ab2e4b7387230ea0
thdrjsdn98 <74442674+thdrjsdn98@users.noreply.github.com>
2026-09-10 08:02:01 +0900

    Add files via upload

--- magnetism/sw.js
+++ magnetism/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'electric-magnetism-cache-v2';
+const CACHE_NAME = 'electric-magnetism-cache-v3';
 const FILES_TO_CACHE = [
   './', './index.html', './style.css', './script.js', './questions.js',
   './manifest.json', './icon-192.png', './icon-512.png'
