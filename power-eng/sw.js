dffd60fd7a2e7cfa5b1ea5bf284dcd57cda4808b
thdrjsdn98 <74442674+thdrjsdn98@users.noreply.github.com>
2026-09-10 08:02:25 +0900

    Add files via upload

--- power-eng/sw.js
+++ power-eng/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'electric-power-cache-v2';
+const CACHE_NAME = 'electric-power-cache-v3';
 const FILES_TO_CACHE = [
   './', './index.html', './style.css', './script.js', './questions.js',
   './manifest.json', './icon-192.png', './icon-512.png'
